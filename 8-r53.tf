9e848ea265a5d61905713ee7cf3bbe4166f840b8
Sirupa-Rakesh <siruparakeshrakhi@gmail.com>
2026-07-22 11:03:19 +0530

    jenkins-all-new-version

--- 8-r53.tf
+++ 8-r53.tf
@@ -2,8 +2,9 @@ resource "aws_route53_record" "jenkins" {
   zone_id = data.aws_route53_zone.domain.zone_id
   name    = "${var.subdomain}.${var.domain_name}"
   type    = "A"
-  ttl     = 300
+  ttl     = 3
   records = [aws_eip.Rakesh[0].public_ip]
+  allow_overwrite = true
 }
 
 resource "aws_route53_record" "sonarqube" {
@@ -12,7 +13,18 @@ resource "aws_route53_record" "sonarqube" {
   zone_id = data.aws_route53_zone.domain.zone_id
   name    = "sonar.${var.domain_name}"
   type    = "A"
-  ttl     = 300
-
+  ttl     = 3
   records = [aws_eip.sonarqube[count.index].public_ip]
+  allow_overwrite = true
+}
+
+resource "aws_route53_record" "jenkins_agent" {
+  count = var.jenkins ? 1 : 0
+  zone_id = data.aws_route53_zone.domain.zone_id
+  name    = "jenkins-agent.${var.domain_name}"
+  type    = "A"
+  ttl     = "1"
+  records = [aws_instance.jenkins_agent[0].private_ip]
+  allow_overwrite = true
+  
 }
